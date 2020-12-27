afecf8dcdbf7ce63f2f59ec46894d2ce59b67616
Maksim N. Brykov <mixmugz@gmail.com>
2020-12-27 15:26:17 +0300

    - Fixed for SimUpdate 2 - Reworked function calls

---
+++ uimod-mugz-hiddentooltip/html_ui/mJS/mugzTips.js
@@ -0,0 +1,11 @@
+﻿var tipsKey = 18;
+window.document.addEventListener("keydown", function (e) {
+  if (e.keyCode == tipsKey) {
+    window.document.documentElement.style.setProperty('--tips-block', 'block');
+  }
+});
+window.document.addEventListener("keyup", function (e) {
+  if (e.keyCode == tipsKey) {
+    window.document.documentElement.style.setProperty('--tips-block', 'none');
+  }
+});
