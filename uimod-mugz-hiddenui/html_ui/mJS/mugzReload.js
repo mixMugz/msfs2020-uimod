ec10626333be8a1fe9650fa4765f6741e8de2f36
Maksim N. Brykov <mixmugz@gmail.com>
2022-01-15 16:21:25 +0300

    Changed to window location reload

--- uimod-mugz-hiddenui/html_ui/mJS/mugzReload.js
+++ uimod-mugz-hiddenui/html_ui/mJS/mugzReload.js
@@ -2,7 +2,7 @@ window.document.addEventListener("keyup", function (e) {
   if (e.keyCode == iKey || e.keyCode == mKey) {
     setTimeout(function() { 
       if (localStorage.getItem('iToggle') == 'false') {
-        document.location.reload();
+        window.location.reload();
       }
     }, 50);
   }
