9de36296842710a11c81c8c18de700bba0d8032a
Maksim N. Brykov <mixmugz@gmail.com>
2022-01-15 19:54:50 +0300

    Removed unusable

--- uimod-mugz-hiddenui/html_ui/mJS/mugzReload.js
+++
@@ -1,9 +0,0 @@
-window.document.addEventListener("keyup", function (e) {
-  if (e.keyCode == iKey || e.keyCode == mKey) {
-    setTimeout(function() { 
-      if (localStorage.getItem('iToggle') == 'false') {
-        window.location.reload();
-      }
-    }, 50);
-  }
-});
